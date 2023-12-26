41216b333ba65e429637ff2853cb535ec36be2b6
Christmark <141054674+Christmark@users.noreply.github.com>
2023-12-26 06:40:16 +0000

    Task Sheet 1-1-2

---
+++ ArithmeticTask.java
@@ -0,0 +1,24 @@
+class ArithmeticTask {
+
+    public static void main (String[] args){
+          
+        int result = 1 + 2; // result is now 3
+        System.out.println("Result After Addition : " + result);
+
+        result = result - 1; // result is now 2
+        System.out.println("Result after subtraction: " + result);
+
+        result = result * 2; // result is now 4
+        System.out.println("Result after multiplication: " +result);
+
+        result = result / 2; // result is now 2
+        System.out.println("Result after division: " + result);
+
+        result = result + 8; // result is now 10
+        System.out.println("Result After Addition : " + result);
+
+        result = result % 7; // result is now 3
+        System.out.println("Final result: "+ result);
+
+    }
+}
